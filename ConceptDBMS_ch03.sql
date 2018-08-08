04857017790ac5c76dc2b6e556c6f3a73a8c4b1f
Vagacoder <qiruihu@gmail.com>
2018-08-07 22:37:32 -0700

    update a file

--- ConceptDBMS_ch03.sql
+++ ConceptDBMS_ch03.sql
@@ -14,12 +14,18 @@ select count(*), sum(cust_id) from customers where cust_id
 
 -- grouping 
 -- group by clause
-select order_num, order_date, count(order_num) 
+-- please note error 1055, only full group
+select  cust_id, count(order_num) 
 	from orders group by cust_id;
 -- having clause
-select order_num, order_date, count(order_num) 
+select cust_id, count(order_num) 
 	from orders group by cust_id having count(order_num) >1;
-
-
     
+-- join table
+-- 1. SELECT--FROM--WHERE col_from_t1 = col_from_t2
+
+select customers.cust_id, cust_name, cust_contact, order_num, order_date 
+from customers, orders 
+where customers.cust_id = orders.cust_id
+order by customers.cust_id;
 
