8680ae4dfbdcc90044730829c9a467bb27e54053
Vagacoder <30400336+Vagacoder@users.noreply.github.com>
2018-08-08 16:36:00 -0700

    update a file

--- ConceptDBMS_ch03.sql
+++ ConceptDBMS_ch03.sql
@@ -20,6 +20,27 @@ select order_num, order_date, count(order_num)
 select order_num, order_date, count(order_num) 
 	from orders group by cust_id having count(order_num) >1;
 
+-- union
+-- union 1: city = 'Houston' with 2: customers who have order on orders table.
+select cust_id, cust_name from customers where cust_city = 'Houston'
+union
+select customers.cust_id, cust_name from customers, orders
+where customers.cust_id = orders.cust_id;
 
-    
+-- update a table 
+insert into orders (order_num, order_date, cust_id)
+values (20010, '2012-05-01', 1000000010 );
+
+update orders set cust_id = 1000000011 where order_num = 20010;
+
+delete from orders where order_num = 20010;
+
+-- create table frmo a query
+-- INTO clause does not work in MySQL
+select * into ca_customers from customers where cust_state = 'CA' ;
+-- try create table
+create table ca_customers select * from customers where cust_state = 'CA';
+select * from ca_customers;
+select count(order_num) as cust_count from orders where cust_id = 1000000001;
+
 
